870450d8514e970e57b7c604be06eec8dc1bd503
John Michael Maxian <73067227+JohnMichael0910@users.noreply.github.com>
2021-06-01 22:16:54 +0800

    Add files via upload

---
+++ config/config.php
@@ -0,0 +1,6 @@
+<?php
+	define('ROOT_URL', 'guestbook-login.php');
+	define('DB_HOST', 'sql6.freesqldatabase.com');
+	define('DB_USER', 'sql6416381');
+	define('DB_PASS', 'tvq3lasRAL');
+	define('DB_NAME', 'sql6416381');
